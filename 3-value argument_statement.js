a970408cb5bdae6e8fc52cbec3ceafedb5047e44
Hope Nyateya <nyateyah@gmail.com>
2025-05-28 13:51:14 +0300

    const added

--- 3-value argument_statement.js
+++ 3-value argument_statement.js
@@ -1,5 +1,7 @@
+const firstArgument = process.argv[2];
+
 if (firstArgument === undefined) {
-  console.log("No Argument")
+  console.log("No Argument");
 } else {
-  console.log(firstArgument)
+  console.log(firstArgument);
 }
