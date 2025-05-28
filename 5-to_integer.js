4c3f9bee4f9255188d4f7916eba4ff39401d856a
Hope Nyateya <nyateyah@gmail.com>
2025-05-28 14:26:23 +0300

    new args

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,5 +1,10 @@
-if (isNaN(persedint)){
-  console.log("Not a number");
+const [arg] = process.argv;
+const num = Number(arg)
+
+
+if (!isNaN(num)){
+
+  console.log(`My number: ${num}`);
 } else {
-  console.log(`My number: ${persedint}`);
+  console.log('Not a number');
 }
