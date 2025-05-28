56f24ae99f326480a6a0257db8fb6332019376e5
Hope Nyateya <nyateyah@gmail.com>
2025-05-28 14:51:20 +0300

    syntax corrected

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -4,7 +4,7 @@ const numOccurences = Number(arg);
 if (isNaN(numOccurences)){
   console.log("Missing number of occurrences");
 } else {
-  let outputString = "";
-}for(let i = 0; i< numOccurences; i++) {
-  
-}console.log('C is fun');
+for(let i = 0; i< numOccurences; i++) {
+  console.log('C is fun');
+  }
+} 
